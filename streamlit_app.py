2ecdd49a58c2b7ea0a5d706066d331076aad8305
anhle32 <90089985+anhle32@users.noreply.github.com>
2024-07-11 10:16:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def set_background(png_file):
     <style>
     .stApp {
         background: url("data:image/png;base64,%s") no-repeat right bottom;
-        background-size: 600px 600px; /* Adjust the size of the background image */
+        background-size: 600px 700px; /* Adjust the size of the background image */
     }
     .stRadio > div {
         display: flex;
